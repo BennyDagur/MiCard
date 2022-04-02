b5c695c563b964a9ec40d1cbdb5a3708cababaee
BennyDagur <bdg2@nemandi.ntv.is>
2022-04-02 18:50:14 +0000

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,47 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+  MyApp()
   );
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+
+                  width: 100,
+                  height: double.infinity,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                      radius: 50,
+                        backgroundColor: Colors.blue,
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        child: Text('Benoný Dagur Guðmundsson'),
+                      ),
+                    ]
+                ),
+                Container(
+                width: 100,
+                height: double.infinity,
+                )
+              ],
+            ),
+        ),
+      ),
+    );
+  }
+  }
